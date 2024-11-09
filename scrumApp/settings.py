875b414539060bc747da560334751dfafc3c2e07
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-11-09 00:19:29 -0500

    update database settings to use PostgreSQL with environment variable for password

--- scrumApp/settings.py
+++ scrumApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'scrumApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'scrumbacktasks-uninorte-a287.d.aivencloud.com',
+        'PORT': '21121',
     }
 }
 
